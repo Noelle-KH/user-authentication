515f0b9b1251dfcd1ae1581246fd08b76aac4c3a
Noelle-KH <noelletang86@gmail.com>
2022-11-22 19:33:57 +0800

    feat: add express-handlebars and layout

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
 const port = 3000
 
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello</h1>')
+  res.render('index')
 })
 
 app.listen(port, () => {
